29cc0c8e91dcb1c2adc232881600368884802861
snOOpyCodesss <snoopy140224@gmail.com>
2024-08-02 01:27:08 +0530

    local db fixed by sqlite

--- src/actions/action.ts
+++ src/actions/action.ts
@@ -1,6 +1,16 @@
 'use server';
+
+import prisma from "@/lib/db";
+
 export async function createPost(formData: FormData){
     const title = formData.get("title") as string;
     const body = formData.get("body") as string;
-    console.log(title, body);
+    
+    //update db
+    await prisma.post.create({
+        data: {
+            title,
+            body,
+        },
+    });
 }
